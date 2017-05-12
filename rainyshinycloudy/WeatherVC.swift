b95691d0a42b863206bbff810afe6992c83e9a75
Jing Gao <jinggao@Jings-MacBook-Pro-2.local>
2017-05-12 16:28:28 +1000

    interface finished

---
+++ rainyshinycloudy/WeatherVC.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherVC.swift
+//  rainyshinycloudy
+//
+//  Created by Jing Gao on 12/5/17.
+//  Copyright © 2017 Jing Gao. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
