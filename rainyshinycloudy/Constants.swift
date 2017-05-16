144812c3f879d6a282c49736528f48be357a5584
Jing Gao <jinggao@Jings-MacBook-Pro-2.local>
2017-05-16 15:38:16 +1000

    data download successfully

--- rainyshinycloudy/Constants.swift
+++ rainyshinycloudy/Constants.swift
@@ -14,4 +14,6 @@ let LONGITUDE = "&lon="
 let APP_ID = "&appid="
 let API_KEY = "cae256577bb9ad57f3547a9ea3359b10"
 
+typealias DownloadComplete = () -> ()
+
 let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(API_KEY)"
